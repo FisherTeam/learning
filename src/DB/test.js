ad45b3293da6f463d5fc7ba1d2cfff7727ee0638
张博群 <383088696@qq.com>
2017-05-08 13:03:59 +0800

    shit

--- src/DB/test.js
+++ src/DB/test.js
@@ -1,7 +1,23 @@
 var MongoClient = require('mongodb').MongoClient;
-var DB_CONN_STR = 'mongodb://localhost:27017/test';
+var sprintf = require("sprintf-js").sprintf;
+var DB_CONN_STR = 'mongodb://root:zbq1992-@dds-m5ec27fea7722c841.mongodb.rds.aliyuncs.com:3717';
 
-MongoClient.connect(DB_CONN_STR, function(err, db) {
+var host1 = "dds-m5ec27fea7722c841.mongodb.rds.aliyuncs.com";
+var port1 = 3717;
+var host2 = "dds-m5ec27fea7722c842.mongodb.rds.aliyuncs.com";
+var port2 = 3717;
+var username = "root";
+var password = "zbq1992-";
+var replSetName = "mgset-3270637";
+var demoDb = "test";
+var demoColl = "testColl";
+// 官方建议使用的方案
+var url_str = sprintf("mongodb://%s:%d,%s:%d/%s?replicaSet=%s", host1, port1, host2, port2, demoDb, replSetName);
+
+console.info("url:", url_str);
+MongoClient.connect(url_str, function(err, db) {
+    if (err) { console.log(err) }
+    console.log(db)
     db.collection('site').remove({ 'name': "zbq" })
     db.collection('site').insert([
         { name: 'zbq', age: '20' }
