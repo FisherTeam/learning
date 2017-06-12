5e4efb4ae96c506e87419588a95b86bcba39ecf0
张博群 <383088696@qq.com>
2017-06-12 14:20:03 +0800

    ts

---
+++ greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = 123;
+document.body.innerHTML = greeter(user);
---
+++ greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person:string){
+    return "Hello, " + person;
+}
+
+
+var user = 123;
+document.body.innerHTML = greeter(user); 
